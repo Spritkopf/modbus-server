0cef10ecdfd511a172ea419ce90726e9377659de
R. Beck <spritkopf@gmail.com>
2026-01-12 21:16:08 +0100

    add feature: read input registers

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,29 @@
 #![no_std]
 
 use modbus_core::{
+    Coils, Data, Error, Request, Response, ResponsePdu,
     rtu::{
-        server::{decode_request, encode_response}, Header, ResponseAdu
-    }, Coils, Data, Error, Request, Response, ResponsePdu
+        Header, ResponseAdu,
+        server::{decode_request, encode_response},
+    },
 };
 // TODO: add library error type
 
 pub trait ModbusHandler {
     fn read_coils(&mut self, addr: usize, len: usize, out: &mut [bool]) -> Result<usize, Error>;
-    fn read_holding_registers(&mut self, addr: usize, len: usize, out: &mut [u16]) -> Result<usize, Error>;
-    fn on_write(&mut self, addr: usize, len: usize, buf: &[bool]) -> Result<usize, Error>;
+    fn read_holding_registers(
+        &mut self,
+        addr: usize,
+        len: usize,
+        out: &mut [u16],
+    ) -> Result<usize, Error>;
+    fn read_input_registers(
+        &mut self,
+        addr: usize,
+        len: usize,
+        out: &mut [u16],
+    ) -> Result<usize, Error>;
+    fn write_coils(&mut self, addr: usize, len: usize, buf: &[bool]) -> Result<usize, Error>;
 }
 
 pub struct ModbusRtuServer<H> {
@@ -23,10 +36,7 @@ where
     H: ModbusHandler,
 {
     pub fn new(unit_id: u8, handler: H) -> Self {
-        Self {
-            unit_id,
-            handler,
-        }
+        Self { unit_id, handler }
     }
 
     pub fn process_frame(&mut self, rx: &[u8], tx: &mut [u8]) -> Result<usize, Error> {
@@ -39,7 +49,7 @@ where
                     let mut coils_buf = [false; 255];
 
                     // call user handler for read_coils
-                    let handler_result =
+                    let _handler_result =
                         self.handler
                             .read_coils(addr as usize, len as usize, &mut coils_buf)?;
 
@@ -55,16 +65,42 @@ where
                     return Ok(tx_len as usize);
                 }
                 Request::ReadHoldingRegisters(addr, len) => {
+                    // todo: refactoring, check if input and holding registers can be matched
+                    // together since they duplicate a lot of code
                     let mut buf = [0u8; 512];
                     let mut reg_buf = [0u16; 255]; //todo: how much memory is max needed?
 
                     // call user handler for read_holding_registers
-                    let handler_result =
-                    self.handler
-                        .read_holding_registers(addr as usize, len as usize, &mut reg_buf)?;
+                    let _handler_result = self.handler.read_holding_registers(
+                        addr as usize,
+                        len as usize,
+                        &mut reg_buf,
+                    )?;
 
                     let data = Data::from_words(&reg_buf[..len as usize], &mut buf)?;
                     let response = Response::ReadHoldingRegisters(data);
+                    let response_adu = ResponseAdu {
+                        hdr: Header {
+                            slave: self.unit_id,
+                        },
+                        pdu: ResponsePdu(Ok(response)),
+                    };
+                    let tx_len = encode_response(response_adu, tx).ok().unwrap();
+                    return Ok(tx_len as usize);
+                }
+                Request::ReadInputRegisters(addr, len) => {
+                    let mut buf = [0u8; 512];
+                    let mut reg_buf = [0u16; 255]; //todo: how much memory is max needed?
+
+                    // call user handler for read_holding_registers
+                    let _handler_result = self.handler.read_input_registers(
+                        addr as usize,
+                        len as usize,
+                        &mut reg_buf,
+                    )?;
+
+                    let data = Data::from_words(&reg_buf[..len as usize], &mut buf)?;
+                    let response = Response::ReadInputRegisters(data);
                     let response_adu = ResponseAdu {
                         hdr: Header {
                             slave: self.unit_id,
@@ -79,7 +115,7 @@ where
 
                     // call user handler for read_coils
                     let num_written_coils =
-                        self.handler.on_write(addr as usize, 1, &coils_buf)?;
+                        self.handler.write_coils(addr as usize, 1, &coils_buf)?;
 
                     // workaround for bug in modbus-core crate: not encoding a response because the
                     // Response::WriteSingleCoil enum is not correct. Since the modbus spec states the response is an
@@ -137,7 +173,7 @@ mod tests {
     ];
 
     impl ModbusHandler for TestData {
-        fn on_write(&mut self, addr: usize, len: usize, buf: &[bool]) -> Result<usize, Error> {
+        fn write_coils(&mut self, addr: usize, len: usize, buf: &[bool]) -> Result<usize, Error> {
             // The variant below iterates through the written coils, this way the application
             // could write states to peripherals when the coils are not buffered in memory...
             for (i, slot) in buf.iter().take(len).enumerate() {
@@ -148,7 +184,12 @@ mod tests {
             Ok(len)
         }
 
-        fn read_coils(&mut self, addr: usize, len: usize, out: &mut [bool]) -> Result<usize, Error> {
+        fn read_coils(
+            &mut self,
+            addr: usize,
+            len: usize,
+            out: &mut [bool],
+        ) -> Result<usize, Error> {
             // manual memcopy since we have the coils states buffered in memory
             out[..len].copy_from_slice(&TEST_COILS[addr..addr + len]);
 
@@ -162,7 +203,24 @@ mod tests {
             Ok(len)
         }
 
-        fn read_holding_registers(&mut self, addr: usize, len: usize, out: &mut [u16]) -> Result<usize, Error> {
+        fn read_holding_registers(
+            &mut self,
+            addr: usize,
+            len: usize,
+            out: &mut [u16],
+        ) -> Result<usize, Error> {
+            // manual memcopy since we have the registers buffered in memory
+            out[..len].copy_from_slice(&TEST_REGISTERS[addr..addr + len]);
+
+            Ok(len)
+        }
+
+        fn read_input_registers(
+            &mut self,
+            addr: usize,
+            len: usize,
+            out: &mut [u16],
+        ) -> Result<usize, Error> {
             // manual memcopy since we have the registers buffered in memory
             out[..len].copy_from_slice(&TEST_REGISTERS[addr..addr + len]);
 
@@ -233,14 +291,42 @@ mod tests {
             0x54, 0x08, // CRC16 (low byte first)
         ];
         let expected_response: [u8; 13] = [
-            0x01, // Slave Address 
+            0x01, // Slave Address
             0x03, // Function Code
             0x08, // Byte Count
-            0x27, 0x10, // Data byte 0: 10000 
-            0x2B, 0x67, // Data byte 1: 11111 
-            0x2B, 0xD6, // Data byte 2: 11222 
-            0x82, 0x35, // Data byte 3: 33333 
+            0x27, 0x10, // Data byte 0: 10000
+            0x2B, 0x67, // Data byte 1: 11111
+            0x2B, 0xD6, // Data byte 2: 11222
+            0x82, 0x35, // Data byte 3: 33333
             0xBC, 0x90, //CRC
+        ];
+        let mut tx_buf = [0u8; 32];
+
+        let len = server.process_frame(&frame, &mut tx_buf).unwrap();
+        let response = &tx_buf[..len];
+        assert_eq!(len, expected_response.len());
+        assert_eq!(response, expected_response);
+    }
+
+    #[test]
+    fn read_input_registers() {
+        let mycoil = TestData {
+            test_coils: [false; 12],
+            test_registers: [0; 12],
+        };
+        let mut server = ModbusRtuServer::new(1, mycoil);
+
+        let frame: [u8; 8] = [
+            0x01, // Slave address
+            0x04, // Function code: Read Input Registers
+            0x00, 0x00, // Starting address: 0
+            0x00, 0x0C, // Quantity of coils: 12
+            0xF0, 0x0F, // CRC16 (low byte first)
+        ];
+        let expected_response: [u8; 29] = [
+            0x01, 0x04, 0x18, 0x00, 0x00, 0x00, 0x02, 0x00, 0x64, 0x03, 0xE8, 0x0D, 0x80, 0x27,
+            0x10, 0x2B, 0x67, 0x2B, 0xD6, 0x82, 0x35, 0xA0, 0x1B, 0xD9, 0x7E, 0xFF, 0xFF, 0x21,
+            0x74,
         ];
         let mut tx_buf = [0u8; 32];
 
